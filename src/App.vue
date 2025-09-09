cf58710a019847668b0b393e7d75d8b9a6042901
mateusramos123 <mateus.oliveira.ifc@gmail.com>
2025-09-09 16:10:22 -0300

    FEAT: Categoria Feita #27

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-import CategoriaComponent from './components/CategoriaComponent.vue';
 </script>
 
 <template>
-   <CategoriaComponent />
+  <RouterView />
+  <h1></h1>
 </template>
 
 <style scoped>
