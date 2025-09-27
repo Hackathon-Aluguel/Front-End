6bb7052368c598bdad7a0f0a6137fcb5a6cae21b
renan1811 <rennlima477@gmail.com>
2025-09-27 11:49:23 -0300

    FEAT: atualizando pagina do usuario ligada com back #61

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -18,10 +18,8 @@ const carregarProdutos = async () => {
   try {
     const response = await axios.get('http://127.0.0.1:8000/api/itens/')
     produtosBackend.value = response.data
-    console.log('Produtos carregados:', response.data)
   } catch (error) {
     console.error(error.response?.data || error.message)
-    alert('Erro ao carregar produtos')
   }
 }
 
